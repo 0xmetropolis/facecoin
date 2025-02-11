c9f625eeedd9496fc9fdce2cdf3b4e470011ee95
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2025-02-11 15:06:57 -0700

    fix loading avatar

--- components/avatar/avatar.tsx
+++ components/avatar/avatar.tsx
@@ -11,41 +11,34 @@ export const Avatar = ({ user }: { user?: User }) => {
     <div className="flex flex-col gap-2 w-32">
       <div className="relative aspect-square">
         <Image
-          className={cn(
-            "rounded-full aspect-square object-cover",
-            isLoading && "animate-pulse opacity-50"
-          )}
+          className={cn("rounded-full aspect-square object-cover")}
           alt={user?.socialHandle || "loading user..."}
           src={
             !isLoading && user?.pfp
               ? `${user.pfp}?lastmod=${new Date().toISOString()}`
               : "/facebook-avatar.webp"
           }
+          priority
           fill
         />
       </div>
       <div className="flex flex-col items-center">
-        <div className="flex flex-row items-center text-black font-semibold">
+        <div className="flex flex-row items-center text-black font-semibold justify-center">
           <p>@</p>
           {isLoading ? (
-            <Skeleton className="w-14 h-4 inline-flex ml-1" />
+            <Skeleton className="w-14 h-5 m-0.5 inline-flex" />
           ) : (
             <p>{user?.socialHandle}</p>
           )}
         </div>
-        <div
-          className={cn(
-            "flex flex-col items-center gap-1",
-            isLoading && "hidden"
-          )}
-        >
-          <p className="text-black whitespace-break-spaces text-center">
-            {user?.tokenAllocation_wei
-              ? formatEther(BigInt(user.tokenAllocation_wei))
-              : "0"}{" "}
-            $facecoin
-          </p>
-          <p className="text-black">($3000)</p>
+        <div className={cn("flex flex-col items-center gap-1")}>
+          {isLoading ? (
+            <Skeleton className="w-24 h-5 inline-flex m-0.5" />
+          ) : user?.tokenAllocation_wei ? (
+            <p className="text-black whitespace-break-spaces text-center">
+              {`${formatEther(BigInt(user.tokenAllocation_wei))} $facecoin`}
+            </p>
+          ) : null}
         </div>
       </div>
     </div>
