9814cb724ed92562bc5e6ec3a38c7e354b33d762
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2025-02-26 15:38:55 -0700

    fix: use value

--- components/avatar/balance-info.tsx
+++ components/avatar/balance-info.tsx
@@ -11,9 +11,10 @@ export const BalanceInfo = ({ user }: { user?: User }) => {
   if (isLoading) return <Skeleton className="w-24 h-5 inline-flex m-0.5" />;
   if (!balanceInfo) return null;
 
+  const useValue = false; // !!balanceInfo.value;
   // display the usd price / the facecoin amount
-  const text = balanceInfo.value
-    ? `$${balanceInfo.value.toLocaleString()}`
+  const text = useValue
+    ? `$${balanceInfo.value!.toLocaleString()}`
     : `${balanceInfo.balance.toLocaleString()} $facecoin`;
 
   return (
